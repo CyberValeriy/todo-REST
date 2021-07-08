5e00c23fe6291f2dbee6f3da9770e8c868c0c4fe
CyberValeriy <grinvalera@met.ua>
2021-07-08 12:51:34 +0300

    Response function usage fix

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,7 +1,7 @@
 const Task = require("../models/task");
 
-const errorHandler = (status,message,res)=>{
-res.status(status).json({message:message});
+const response = (status,message,res,body = {})=>{
+res.status(status).json({message:message,body:body});
 }
 
 exports.postAddTask = async (req, res) => {
@@ -14,10 +14,9 @@ exports.postAddTask = async (req, res) => {
       description: description,
     }).save();
   } catch (err) {
-    return errorHandler(500,err,res)
+    return response(500,err,res)
   }
-
-  res.status(200).json({ message: "Task created!" });
+  response(200,"Task Created!",res);
 };
 
 exports.getTasks = async (req, res) => {
@@ -25,37 +24,35 @@ exports.getTasks = async (req, res) => {
   try {
     tasks = await Task.find();
   } catch (err) {
-    return errorHandler(500,"Fetching failed",res)
+    return response(500,"Fetching failed",res)
   }
-
   const message = tasks.length == 0 ? "No tasks" : "";
-  res.status(200).json({ message: message, body: tasks });
+  response(200,message,res,tasks);
 };
 
 exports.putUpdateTask = async (req, res) => {
   const taskId = req.body.taskId;
   if (!taskId) {
-    return errorHandler(404,'Invalid task id',res);
+    return response(404,'Invalid task id',res);
   }
   const update = { title: req.body.title || "No title", description: req.body.description || "No description" };
-
   try {
     await Task.findByIdAndUpdate(taskId, update, { new: true });
   } catch (err) {
-    return errorHandler(500,'Cast failed',res);
+    return response(500,'Cast failed',res);
   }
-  res.status(201).json({ message: "Task updated!" });
+  response(201,'Task updated!',res);
 };
 
 exports.deleteTask = async (req, res) => {
   const taskId = req.body.taskId;
   if(!taskId){
-      return errorHandler(404,'No id!',res)
+      return response(404,'No id!',res)
   }
   try {
     await Task.findByIdAndDelete(taskId);
   } catch (err) {
-    return errorHandler(404,'Cast failed!',res);
+    return response(404,'Cast failed!',res);
   }
-  res.status(200).json({ message: "Task deleted!" });
+  response(200,'Task deleted',res);
 };
